255822734284dc328dcfeb0d023a55a3131885ef
Robert Moses <rmoses@titaniumbytes.com>
2024-02-15 13:01:41 -0600

    Referencing Webi Install location

--- scripts/webi.sh
+++ scripts/webi.sh
@@ -4,7 +4,7 @@
 curl -sS https://webi.sh/webi | sh
 
 # Install Webi Packages
-xargs webi < ../config_files/webi_packages.list
+xargs ~/.local/bin/webi < ../config_files/webi_packages.list
 
 # Remove Webi Install from Downloads
 rm -rf ~/Downloads/webi
